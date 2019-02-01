f2626dc6c8f935d5bd82c0b5d0e5ee661f595e0c
Jianxuan Tan <jianxuat@usc.edu>
2019-01-31 21:57:05 -0800

    added mobile nav bar

---
+++ js/menu.js
@@ -0,0 +1,38 @@
+// mobile menu bar
+(function () {
+    let state = false;
+
+    // cache DOM
+    let $menu = $('.menu--btn');
+    let $menu_bar1 = $menu.find('.menu--btn__line:nth-of-type(1)');
+    let $menu_bar2 = $menu.find('.menu--btn__line:nth-of-type(2)');
+    let $menu_bar3 = $menu.find('.menu--btn__line:nth-of-type(3)');
+    let $menu_items = $('.menu--items');
+
+    // bind events
+    $menu.click(() => state ? close() : open());
+
+    $('body > div').click(() => close());
+    $('footer').click(() => close());
+    $menu_items.find('a').click(() => close());
+
+    function open() {
+        console.log('open');
+        state = true;
+        $menu_bar1.removeClass('menu--btn__line__exit_1').addClass('menu--btn__line__select_1');
+        $menu_bar2.removeClass('menu--btn__line__exit_2').addClass('menu--btn__line__select_2');
+        $menu_bar3.removeClass('menu--btn__line__exit_3').addClass('menu--btn__line__select_3');
+        $menu_items.css('display', 'block');
+    }
+
+    function close() {
+        if (!state) return;
+        console.log('close');
+        state = false;
+        $menu_bar1.removeClass('menu--btn__line__select_1').addClass('menu--btn__line__exit_1');
+        $menu_bar2.removeClass('menu--btn__line__select_2').addClass('menu--btn__line__exit_2');
+        $menu_bar3.removeClass('menu--btn__line__select_3').addClass('menu--btn__line__exit_3');
+        $menu_items.css('display', 'none');
+    }
+
+})();
