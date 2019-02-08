99966b20b987ab19f7995d3a62779698b991ea95
Jianxuan Tan <jianxuat@usc.edu>
2019-02-07 16:43:31 -0800

    able to swipe on mobile for index carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function jumpto(div) {
     var target = $(div);
     var distance = Math.abs(target.offset().top - $(document).scrollTop()) / screen.height;
@@ -16,3 +15,54 @@ function jumpto(div) {
         }
     });
 }
+
+// carousel
+(function () {
+    let $el = $('#carouselControls');
+    $el.on('slid.bs.carousel', function () {
+        let group = $('.carousel-item.active').attr('group');
+        $('.preview .controls div button').removeClass('selected');
+        $('.preview .controls div button[group="' + group + '"]').addClass('selected');
+    });
+    $('.preview .controls div button').click(function () {
+        let group = $(this).attr('group');
+        let index = $('.preview .carousel-inner [group="' + group + '"]').index();
+        console.log(group + " " + index);
+        $el.carousel(index);
+    });
+    $el.carousel('pause');
+})();
+
+
+// swiping for the carousel
+(function () {
+    let startPos;
+    let time = -1;
+    let currentPos;
+
+    let $el = $('.show');
+    let $carousel = $('#carouselControls');
+
+    $el.on('touchstart', function (e) {
+        time = Date.now();
+        startPos = e.originalEvent.touches[0].pageX;
+    });
+    $el.on('touchend', function (e) {
+        console.log('touchend');
+        let now = Date.now();
+        if (time > 0 && now - time < 1000) {
+            if (currentPos - startPos > 50) {
+                console.log('Swipe Right!!!');
+                $carousel.carousel('prev');
+            } else if (startPos - currentPos > 50) {
+                console.log('Swipe Left!!!');
+                $carousel.carousel('next');
+            }
+        }
+        time = -1;
+    });
+    $el.on('touchmove', function (e) {
+        currentPos = e.originalEvent.touches[0].pageX;
+    });
+
+})();
